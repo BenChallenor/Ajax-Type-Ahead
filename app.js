1c2b7ab73762c18e59fdc6b693c2475a3dcbbfcc
Ben <ben.challenor@btopenworld.com>
2018-07-09 19:25:15 +0100

    Add find matches function

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ fetch(endpoint)
   // converts data to a json file
   .then(data => cities.push(...data))
   // spreads in to the array individual cities
+
+function findMatches(wordToMatch, cities) {
+  return cities.filter(place => {
+    const regex = new RegExp(wordToMatch, 'gi');
+    // global and anycase search
+    return place.city.match(regex) || place.state.match(regex)
+    // checks for if either the place or state contain the letters
+  })
+}
