667da18bc47a84a48ffa33835e6de01ca94d6664
Ben <ben.challenor@btopenworld.com>
2018-07-12 17:53:20 +0100

    Add highlights regex values

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ fetch(endpoint)
 function findMatches(wordToMatch, cities) {
   return cities.filter(place => {
     const regex = new RegExp(wordToMatch, 'gi');
-    // global and anycase search
+    // regex used to match, hi = global and anycase search
     return place.city.match(regex) || place.state.match(regex)
     // checks for if either the place or state contain the letters
   });
@@ -22,14 +22,21 @@ function displayMatches() {
   // creates array of displayMatches
   const html = matchArray.map(place => {
     // loops over array, backtick holds list item
+    const regex = new RegExp(this.value, 'gi');
+    const cityName = place.city.replace(regex, `<span
+     class="hl">${this.value}</span>`)
+    const stateName = place.state.replace(regex, `<span
+      class="hl">${this.value}</span>`)
+    // finds what it matched in the regex and highlights that value
+
     return `
   <li>
-  <span class="name">${place.city}, ${place.state}</span>
+  <span class="name">${cityName}, ${stateName}</span>
   <span class="population">${place.population}</span>
   </li>
 `;
-}).join('');
-// turns array in to one string
+  }).join('');
+  // turns array in to one string
   suggestions.innerHTML = html;
 }
 
